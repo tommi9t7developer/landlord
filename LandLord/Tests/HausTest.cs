5e073838f663a6570fe8d2fb329ca68bb36d85c5
tommi9t7developer <tommi9t7@googlemail.com>
2024-09-21 14:21:20 +0200

    Erster Test

---
+++ LandLord/Tests/HausTest.cs
@@ -0,0 +1,20 @@
+﻿using LandLord.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LandLord.Tests
+{
+    public class HausTest
+    {
+        [Fact]
+        public void nameTest()
+        {
+            Haus haus = new Haus("neu");
+            Assert.Equal("neu", haus.Name);
+        }
+    }
+}
